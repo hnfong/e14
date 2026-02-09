79f2bff7cc7559902abdfe64086603da754ed0e7
hnfong <hnfong@none.local>
2026-02-10 00:47:13 +0800

    Various small fixes for batchai

--- batchai/admin.py
+++ batchai/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.TextInferenceRequest)
+class TextInferenceRequestAdmin(admin.ModelAdmin):
+    save_as = True
+
+admin.site.register(models.TextInferenceRequest, TextInferenceRequestAdmin)
