3588bcd495d39a0f45f645b27b1a4d1efdf2a608
hnfong <hnfong@none.local>
2026-03-12 00:12:41 +0800

    Notes in admin interface

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'create_time']
+    list_display_links = ['title']
+    ordering = ['-create_time']
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(is_public=True)
+
+admin.site.register(models.Note, NoteAdmin)
